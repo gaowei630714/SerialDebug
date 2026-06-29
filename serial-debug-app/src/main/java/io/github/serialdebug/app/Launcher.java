770ea0dde7b4d680b819ce9968f9dc1c591e435a
GAOWEI\gaowei <gaoweif@outlook.com>
2026-06-29 23:44:34 +0800

    chore: scaffold multi-module Maven project with parent POM and all module POMs

---
+++ serial-debug-app/src/main/java/io/github/serialdebug/app/Launcher.java
@@ -0,0 +1,9 @@
+package io.github.serialdebug.app;
+
+import io.github.serialdebug.ui.MainApplication;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
---
+++ serial-debug-ui/src/main/java/io/github/serialdebug/ui/MainApplication.java
@@ -0,0 +1,16 @@
+package io.github.serialdebug.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // placeholder - will be implemented later
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
